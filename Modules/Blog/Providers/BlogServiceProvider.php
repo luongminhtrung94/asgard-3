66717378dc9eb9aed7971ad4dd2118c5baa95433
trung luong <>
2018-12-26 11:55:05 +0700

    init

---
+++ Modules/Blog/Providers/BlogServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Modules\Blog\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Core\Traits\CanPublishConfiguration;
+use Modules\Core\Events\BuildingSidebar;
+use Modules\Core\Events\LoadingBackendTranslations;
+use Modules\Blog\Events\Handlers\RegisterBlogSidebar;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    use CanPublishConfiguration;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerBindings();
+        $this->app['events']->listen(BuildingSidebar::class, RegisterBlogSidebar::class);
+
+        $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
+            // append translations
+        });
+    }
+
+    public function boot()
+    {
+        $this->publishConfig('blog', 'permissions');
+
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+    private function registerBindings()
+    {
+// add bindings
+    }
+}
